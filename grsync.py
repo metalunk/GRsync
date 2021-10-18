06d24e11d57774a326c0480c6d9aa09bb1178ab0
metalunk <chiba.r221@gmail.com>
2021-10-18 21:18:20 +0900

    Remove unused options, refactr

--- grsync.py
+++ grsync.py
@@ -1,13 +1,12 @@
 import json
-import argparse
+import logging
 import socket
-import re
 import os
+from http.client import RemoteDisconnected
 from multiprocessing import Pool
 from pathlib import Path
 from urllib.error import URLError
 from urllib.request import urlopen, Request
-from argparse import RawTextHelpFormatter
 
 from tqdm import tqdm
 
@@ -21,30 +20,33 @@ class Importer:
     GR_PROPS = "v1/props"
     SUPPORT_DEVICE = ['RICOH GR II', 'RICOH GR III', 'GR II']
     BATTERY_LOWER_BOUND = 15
+    N_POOL = 4
 
     def __init__(self, destination_dir: Path):
-        self.device = ''
-        self.destination_dir = destination_dir
+        self._device = ''
+        self._destination_dir = destination_dir
+        self._logger = logging.getLogger()
 
-    def run(self, download_all: bool, start_dir: str = None, start_file: str = None):
-        device = self._get_device_name()
-        if device not in self.SUPPORT_DEVICE:
-            print("Your source device '%s' is unknown or not supported." % device)
+    def run(self):
+        self._device = self._get_device_name()
+        if self._device not in self.SUPPORT_DEVICE:
+            print(f"Your source device '{self._device}' is unknown or not supported.")
             return
-        else:
-            self.device = device
 
         if self._get_battery_level() < self.BATTERY_LOWER_BOUND:
             print("Your battery level is less than 15%, please charge it before sync operation.")
             return
 
-        print(f'Downloading photos from {device} to {self.destination_dir}')
-        self._download_photos(download_all, start_dir, start_file)
+        print(f'Downloading photos from \'{self._device}\' to \'{self._destination_dir}\'')
+        self._download_photos()
 
     def shutdown_device(self):
         req = Request(self.GR_HOST + self.SHUTDOWN_URI)
         req.add_header('Content-Type', 'application/json')
-        urlopen(req, "{}")  # todo: This can be None?
+        try:
+            urlopen(req, b'{}')
+        except RemoteDisconnected:
+            return
 
     def _get_device_name(self):
         data = self._download_json(self.GR_HOST + self.GR_PROPS)
@@ -61,52 +63,38 @@ class Importer:
         for dic in data['dirs']:
             for file in dic['files']:
                 p = f'{dic["name"]}/{file}'
-                if not (self.destination_dir / p).exists():
+                if not (self._destination_dir / p).exists():
                     photoList.append(p)
         return photoList
 
-    def _fetch_photo(self, photo_uri):
-        print(f"Downloading '{photo_uri}'")
-        try:
-            if self.device == 'GR2':
-                f = urlopen(self.GR_HOST + photo_uri)
-            else:
-                f = urlopen(self.GR_HOST + self.PHOTO_LIST_URI + '/' + photo_uri)
-
-            if not (self.destination_dir / photo_uri).parent.exists():
-                (self.destination_dir / photo_uri).parent.mkdir()
-            with (self.destination_dir / photo_uri).open("wb") as local_f:
-                local_f.write(f.read())
-            return True
-        except URLError:
-            return False
-
-    N_POOL = 10
-
-    def _download_photos(self, download_all: bool, start_dir: str = None, start_file: str = None):
-        print("Fetching photo list from %s" % self.device)
-        photo_lists = self._get_photo_list()
-        if download_all:
-            total_photo = len(photo_lists)
+    def _fetch_photo(self, photo_uri: str):
+        if self._device == 'GR2':
+            url = f'{self.GR_HOST}{photo_uri}'
         else:
-            start_uri = "%s/%s" % (start_dir, start_file)
-            if start_uri not in photo_lists:
-                print("Unable to find %s in Ricoh %s" % (start_uri, self.device))
-                return
-            else:
-                while True:
-                    if photo_lists[0] != start_uri:
-                        photo_lists.pop(0)
-                    else:
-                        total_photo = len(photo_lists)
-                        break
+            url = f'{self.GR_HOST}{self.PHOTO_LIST_URI}/{photo_uri}'
 
-        print("Start to download photos")
+        try:
+            f = urlopen(url)
+        except URLError:
+            self._logger.warning(f'Failed to get data from \'{url}\'')
+            return
+
+        if not (self._destination_dir / photo_uri).parent.exists():
+            (self._destination_dir / photo_uri).parent.mkdir()
+
+        with (self._destination_dir / photo_uri).open("wb") as local_f:
+            local_f.write(f.read())
+
+    def _download_photos(self):
+        print("Fetching photo list.")
+        photo_lists = self._get_photo_list()
+
+        print("Start to download photos.")
         with Pool(self.N_POOL) as p:
             for _ in tqdm(p.imap_unordered(self._fetch_photo, photo_lists), total=len(photo_lists)):
                 pass
 
-        print("\nAll photos are downloaded.")
+        print("All photos are downloaded.")
 
     @classmethod
     def _download_json(cls, uri):
@@ -124,64 +112,14 @@ class Importer:
 
 
 def main():
-    # setting up argument parser
-    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter, description='''
-GRsync is a handy Python script, which allows you to sync photos from Ricoh GR
-II or III via Wifi. It has been tested on Mac OS X and Ubuntu. It should be able to
-run on any platform that has a Python environment.
-
-It automatically checks if photos already exists in your local drive. Duplicated
-photos will be skipped and only sync needed photos for you.
-
-Simple usage - Download ALL photos from Ricoh GR II or III:
-
-    ./GRsync -a
-
-Advanced usage - Download photos after specific directory and file:
-
-    ./GRsync -d 100RICOH -f R0000005.JPG
-    
-    All photos after 100RICOH/R0000005.JPG will be downloaded, including all
-    following directories (eg. 101RICOH, 102RICOH)
-
-''')
-    parser.add_argument("-a", "--all", action="store_true", help="Download all photos")
-    parser.add_argument("-d", "--dir", help="Assign directory (eg. -d 100RICOH). MUST use with -f")
-    parser.add_argument("-f", "--file",
-                        help="Start to download photos from specific file \n(eg. -f R0000005.JPG). MUST use with -d")
-    options = parser.parse_args()
-
     destination_dir = os.environ.get('DESTINATION_DIR')
     if destination_dir is None:
         destination_dir = Path('.')
     else:
         destination_dir = Path(destination_dir)
 
-    download_all = False
-    start_dir = None
-    start_file = None
-
-    if options.all is True and options.dir is None and options.file is None:
-        download_all = True
-    elif not (options.dir is None) and (options.file is not None) and options.all is False:
-        match = re.match(r"^[1-9]\d\dRICOH$", options.dir)
-        if match:
-            start_dir = options.dir
-        else:
-            print("Incorrect directory name. It should be something like 100RICOH")
-            return
-        match = re.match(r"^R0\d{6}\.JPG$", options.file)
-        if match:
-            start_file = options.file
-        else:
-            print("Incorrect file name. It should be something like R0999999.JPG. (all in CAPITAL)")
-            return
-    else:
-        parser.print_help()
-        return
-
     importer = Importer(destination_dir)
-    importer.run(download_all, start_dir, start_file)
+    importer.run()
 
     print('Shutting down the device.')
     importer.shutdown_device()
